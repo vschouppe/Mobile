f192acea5125f0b7feac6480dfd2459db1278075
vschouppe <vincentschouppe@hotmail.com>
2023-10-20 16:00:59 +1000

    add the mySoothe app which is just some layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ android {
         }
     }
 }
-
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -58,7 +57,10 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.compose.material3:material3-window-size-class:1.1.2")
+    implementation ("androidx.compose.material:material-icons-extended")
+    implementation ("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
