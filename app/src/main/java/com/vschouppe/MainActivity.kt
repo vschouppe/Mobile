38c02cc8812cfb8dd3218d93ab39d3a21b7c4bb0
vschouppe <vincentschouppe@hotmail.com>
2023-10-26 13:00:05 +1000

    TipCalculator app: initial dev calculating an amount entered by user

--- app/src/main/java/com/vschouppe/MainActivity.kt
+++ app/src/main/java/com/vschouppe/MainActivity.kt
@@ -1,46 +1,143 @@
 package com.vschouppe
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import com.vschouppe.ui.theme.MobileAppsPlaygroundTheme
+import androidx.compose.ui.unit.dp
+import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            MobileAppsPlaygroundTheme {
-                // A surface container using the 'background' color from the theme
+            TipTimeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TipTimeLayout()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TipTimeLayout() {
+    var inputAmount  by remember {mutableStateOf("")}
+    var tipamount by remember {mutableStateOf("")}
+    tipamount = calculateTip(inputAmount.toDoubleOrNull() ?: 0.0,15.0)
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+        EditNumberField(
+            value = inputAmount,
+            onValueChange = {
+                inputAmount = it
+            },
+            modifier = Modifier
+            .padding(bottom = 15.dp)
+            .fillMaxWidth())
+        TipAmount(modifier = Modifier,tipamount)
+        Spacer(modifier = Modifier.height(150.dp))
+    }
+}
+
+@Composable
+fun TipAmount(modifier : Modifier, tipAmount : String){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = stringResource(R.string.tip_amount, tipAmount),
+        style = MaterialTheme.typography.displaySmall
     )
+}
+@Preview
+@Composable
+fun TipAmountPreview(){
+    TipAmount(modifier = Modifier, "12.15")
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNumberField(value: String,
+                    onValueChange: (String) -> Unit,
+                    modifier : Modifier){
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(stringResource(id = R.string.input_label_bill_amount))},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+        modifier = Modifier
+    )
+}
+@Preview
+@Composable
+fun EditNumberFieldPreview(){
+    var inputAmount  by remember {mutableStateOf("")}
+    EditNumberField("12",
+        {
+        "doesn't matter here"
+        },
+        modifier = Modifier)
+}
+
+
+/**
+ * Calculates the tip based on the user input and format the tip amount
+ * according to the local currency.
+ * Example would be "$10.00".
+ */
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobileAppsPlaygroundTheme {
-        Greeting("Android")
+fun TipTimeLayoutPreview() {
+    TipTimeTheme {
+        TipTimeLayout()
     }
 }
