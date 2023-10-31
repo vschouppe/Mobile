eea20db38ffe506093a541f2fc819a9456ace3c9
vschouppe <vincentschouppe@hotmail.com>
2023-10-31 16:46:17 +1000

    ArtApp: change appId to be unique. change app icon etc.

--- app/src/main/java/com/vschouppe/ArtApp.kt
+++ app/src/main/java/com/vschouppe/ArtApp.kt
@@ -32,15 +32,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vschouppe.data.ArtInfo
 import com.vschouppe.data.myArtCollection
 import com.vschouppe.ui.theme.MobileAppsPlaygroundTheme
 
-class MainActivity : ComponentActivity() {
+class ArtApp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
